ef7183b5bd7e870f02cc08873a4ab62ff1982673
Sittipong19 <75230330+Sittipong19@users.noreply.github.com>
2021-03-03 12:02:46 +0700

    Add files via upload

---
+++ oop-w12-Sittipong19-main/author/oop-w15/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace oop_w15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAutName = new System.Windows.Forms.TextBox();
+            this.txtAutpicture = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAutId = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAutName
+            // 
+            this.txtAutName.Location = new System.Drawing.Point(110, 107);
+            this.txtAutName.Name = "txtAutName";
+            this.txtAutName.Size = new System.Drawing.Size(100, 20);
+            this.txtAutName.TabIndex = 0;
+            // 
+            // txtAutpicture
+            // 
+            this.txtAutpicture.Location = new System.Drawing.Point(110, 164);
+            this.txtAutpicture.Name = "txtAutpicture";
+            this.txtAutpicture.Size = new System.Drawing.Size(100, 20);
+            this.txtAutpicture.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Picture";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(29, 260);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(119, 260);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 7;
+            this.btnEdit.Text = "edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(212, 260);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(29, 306);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 9;
+            this.btnDisplay.Text = "display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(348, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(338, 298);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "id";
+            // 
+            // txtAutId
+            // 
+            this.txtAutId.Location = new System.Drawing.Point(110, 67);
+            this.txtAutId.Name = "txtAutId";
+            this.txtAutId.Size = new System.Drawing.Size(100, 20);
+            this.txtAutId.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 427);
+            this.Controls.Add(this.txtAutId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAutpicture);
+            this.Controls.Add(this.txtAutName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAutName;
+        private System.Windows.Forms.TextBox txtAutpicture;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAutId;
+    }
+}
+
---
+++ oop-w12-Sittipong19-main/author/oop-w15/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace oop_w15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string MyConnection2 = "datasource=localhost;port=3306;username=root;password=;database=oopdb";
+
+                string Query = "insert into author(aut_name,aut_picture) values('" + this.txtAutName.Text + "','" + this.txtAutpicture.Text + "');";
+
+
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+
+                MySqlDataReader MyReader2;
+
+                MyConn2.Open();
+
+                MyReader2 = MyCommand2.ExecuteReader();  
+
+                MessageBox.Show("Save Data");
+
+                while (MyReader2.Read())
+                {
+
+
+
+                }
+
+                MyConn2.Close();
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+
+                MessageBox.Show(ex.Message);
+
+            } 
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string MyConnection2 = "datasource=localhost;port=3306;username=root;password=;database=oopdb";
+
+                string Query = "select * from author;";
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = MyCommand2;
+
+                DataTable dTable = new DataTable();
+
+                MyAdapter.Fill(dTable);
+                
+                dataGridView1.DataSource = dTable; 
+
+
+
+                // MyConn2.Close(); 
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+
+                MessageBox.Show(ex.Message);
+
+            } 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string MyConnection2 = "datasource=localhost;port=3306;username=root;password=;database=oopdb";
+
+                string Query = "delete from author where aut_id='" + this.txtAutId.Text + "';";
+
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+
+                MySqlDataReader MyReader2;
+
+                MyConn2.Open();
+
+                MyReader2 = MyCommand2.ExecuteReader();
+
+                MessageBox.Show("Data Deleted");
+
+                while (MyReader2.Read())
+                {
+
+
+
+                }
+
+                MyConn2.Close();
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+
+                MessageBox.Show(ex.Message);
+
+            } 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                string MyConnection2 = "datasource=localhost;port=3306;username=root;password=;database=oopdb";
+
+                string Query = "update author set aut_name='" + this.txtAutName.Text + "',aut_picture='" + this.txtAutpicture.Text + "' where aut_id='" + this.txtAutId.Text + "';";
+
+
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+
+                MySqlDataReader MyReader2;
+
+                MyConn2.Open();
+
+                MyReader2 = MyCommand2.ExecuteReader();
+
+                MessageBox.Show("Data Updated");
+
+                while (MyReader2.Read())
+                {
+
+
+
+                }
+
+                MyConn2.Close();//Connection closed here 
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+
+                MessageBox.Show(ex.Message);
+
+            } 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
